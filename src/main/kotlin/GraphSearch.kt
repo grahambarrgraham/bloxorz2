afabf35f2d0675b2bcbfb7f38eedc40b6ba6fac3
gbarr <grahambarrgrahamn@gmail.com>
2020-07-01 17:28:55 +0100

    Added all paths

---
+++ src/main/kotlin/GraphSearch.kt
@@ -0,0 +1,101 @@
+package org.rrabarg
+
+import java.lang.Exception
+import java.util.PriorityQueue
+
+object GraphSearch {
+
+    //An Edge on the graph relative to a source Vertex.
+    data class Edge<V>(val cost: Int, val destination: V)
+
+    //A costed Path through the graph, with an ordered list of each Edge traversed
+    data class Path<V>(val cost: Int = 0, val history: List<Edge<V>> = emptyList())
+
+    class NoPathFound : Exception("No path found")
+
+    /**
+     * Finds the lowest cost path from a source Node, to a sink Node, in a graph of connected Nodes. Each edge
+     * in the graph has an associated cost of traversal, and so the cost of the path from source to sink is defined as
+     * the sum of the edge costs' that were traversed on that path.
+     * If two paths have the same cost, then the one with the least edges traversed is deemed the lowest cost.
+     */
+    fun <Vertex> shortestPath(
+        source: Vertex,
+        isSink: (Vertex) -> Boolean,
+        edges: (Vertex) -> List<Edge<Vertex>>
+    ): Path<Vertex> {
+
+        val queue = PriorityQueue<Path<Vertex>>(compareBy { it.cost })
+        val visited = mutableMapOf<Vertex, Path<Vertex>>()
+
+        fun currentVertex(path: Path<Vertex>) = path.history.lastOrNull()?.destination ?: source
+
+        queue.add(Path())
+        visited[source] = Path()
+
+        while (true) {
+
+            val path = queue.poll() ?: throw NoPathFound()
+            val vertex = currentVertex(path)
+            if (isSink(vertex)) {
+                return path
+            }
+
+            edges(vertex).forEach {
+                val nextPath = Path(path.cost + it.cost, path.history + it)
+                val cheapest = visited[it.destination]?.cost ?: Int.MAX_VALUE
+
+                if (nextPath.cost < cheapest) {
+                    queue.add(nextPath)
+                    visited[vertex] = path
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Finds the all of the paths from a source Node, to a sink Node, in a graph of connected Nodes. Each edge
+     * in the graph has an associated cost of traversal, and so the cost of the path from source to sink is defined as
+     * the sum of the edge costs' that were traversed on that path. The results are sorted in increasing order of cost.
+     * If two paths have the same cost, then the one with the least edges traversed is deemed the lowest cost.
+     */
+    fun <Vertex> allPaths(
+        source: Vertex,
+        isSink: (Vertex) -> Boolean,
+        edges: (Vertex) -> List<Edge<Vertex>>
+    ): List<Path<Vertex>> {
+
+        val queue = PriorityQueue<Path<Vertex>>(compareBy<Path<Vertex>> { it.cost }.thenBy { it.history.size })
+        val visited = mutableMapOf<Vertex, Path<Vertex>>()
+
+        fun currentVertex(path: Path<Vertex>) = path.history.lastOrNull()?.destination ?: source
+
+        queue.add(Path())
+        visited[source] = Path()
+
+        val result = mutableListOf<Path<Vertex>>()
+
+        while (true) {
+
+            val path = queue.poll() ?: break
+            val vertex = currentVertex(path)
+            if (isSink(vertex)) {
+                result.add(path)
+            }
+
+            edges(vertex).forEach {
+                val nextPath = Path(path.cost + it.cost, path.history + it)
+                val cheapest = visited[it.destination]?.cost ?: Int.MAX_VALUE
+
+                if (nextPath.cost < cheapest) {
+                    queue.add(nextPath)
+                    visited[vertex] = path
+                }
+            }
+        }
+
+        return result
+
+    }
+}
---
+++ src/test/kotlin/GraphSearchTest.kt
@@ -0,0 +1,203 @@
+package org.rrabarg
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class GraphSearchTest {
+
+    data class Vertex(val id: String)
+    data class Edge(val v1: Vertex, val v2: Vertex, val cost: Int)
+
+    @Test fun shortestPathHappyCase() {
+
+        val vertices = List(6) { Vertex((it+1).toString()) }
+
+        val edges = listOf(
+            Edge(vertices[0], vertices[1], 7),
+            Edge(vertices[0], vertices[5], 14),
+            Edge(vertices[0], vertices[2], 9),
+            Edge(vertices[1], vertices[2], 10),
+            Edge(vertices[1], vertices[3], 15),
+            Edge(vertices[2], vertices[5], 2),
+            Edge(vertices[2], vertices[3], 11),
+            Edge(vertices[3], vertices[4], 6),
+            Edge(vertices[4], vertices[5], 9)
+        )
+
+        testShortestPath(edges, vertices[0], vertices[4], 20, 3)
+    }
+
+    @Test fun shortestPathSourceAndSinkTheSame() {
+
+        val vertices = List(6) { Vertex((it+1).toString()) }
+
+        val edges = listOf(
+            Edge(vertices[0], vertices[1], 7),
+            Edge(vertices[0], vertices[5], 14),
+            Edge(vertices[0], vertices[2], 9),
+            Edge(vertices[1], vertices[2], 10),
+            Edge(vertices[1], vertices[3], 15),
+            Edge(vertices[2], vertices[5], 2),
+            Edge(vertices[2], vertices[3], 11),
+            Edge(vertices[3], vertices[4], 6),
+            Edge(vertices[4], vertices[5], 9)
+        )
+
+        testShortestPath(edges, vertices[1], vertices[1], 0, 0)
+    }
+
+
+    @Test(expected = GraphSearch.NoPathFound::class) fun shortestPathNoPathToSink() {
+
+        val vertices = List(7) { Vertex((it+1).toString()) }
+
+        val edges = listOf(
+            Edge(vertices[0], vertices[1], 7),
+            Edge(vertices[0], vertices[5], 14),
+            Edge(vertices[0], vertices[2], 9),
+            Edge(vertices[1], vertices[2], 10),
+            Edge(vertices[1], vertices[3], 15),
+            Edge(vertices[2], vertices[5], 2),
+            Edge(vertices[2], vertices[3], 11),
+            Edge(vertices[3], vertices[4], 6),
+            Edge(vertices[4], vertices[5], 9)
+        )
+
+        testShortestPath(edges, vertices[1], vertices[6], 0, 0)
+    }
+
+    @Test fun shortestPathsChoosePrefersLeastEdges() {
+
+        val vertices = List(6) { Vertex((it+1).toString()) }
+
+        val edges = listOf(
+            Edge(vertices[0], vertices[1], 3),
+            Edge(vertices[0], vertices[2], 1),
+            Edge(vertices[1], vertices[2], 2)
+        )
+
+        testShortestPath(edges, vertices[1], vertices[0], 3, 1)
+    }
+
+    @Test fun allPathsHappyCase() {
+
+        val vertices = List(6) { Vertex((it+1).toString()) }
+
+        val edges = listOf(
+            Edge(vertices[0], vertices[1], 7),
+            Edge(vertices[0], vertices[5], 14),
+            Edge(vertices[0], vertices[2], 9),
+            Edge(vertices[1], vertices[2], 10),
+            Edge(vertices[1], vertices[3], 15),
+            Edge(vertices[2], vertices[5], 2),
+            Edge(vertices[2], vertices[3], 11),
+            Edge(vertices[3], vertices[4], 6),
+            Edge(vertices[4], vertices[5], 9)
+        )
+
+        testAllPaths(edges, vertices[0], vertices[4], listOf(
+            ExpectedPath(20, 3),
+            ExpectedPath(23, 2),
+            ExpectedPath(26, 3),
+            ExpectedPath(28, 3),
+            ExpectedPath(34, 4)))
+    }
+
+    @Test fun allPathsNoRoute() {
+
+        val vertices = List(7) { Vertex((it+1).toString()) }
+
+        val edges = listOf(
+            Edge(vertices[0], vertices[1], 7),
+            Edge(vertices[0], vertices[5], 14),
+            Edge(vertices[0], vertices[2], 9),
+            Edge(vertices[1], vertices[2], 10),
+            Edge(vertices[1], vertices[3], 15),
+            Edge(vertices[2], vertices[5], 2),
+            Edge(vertices[2], vertices[3], 11),
+            Edge(vertices[3], vertices[4], 6),
+            Edge(vertices[4], vertices[5], 9)
+        )
+
+        testAllPaths(edges, vertices[0], vertices[6], listOf())
+    }
+
+    @Test fun allPathsPreferLeastEdges() {
+
+        val vertices = List(4) { Vertex((it+1).toString()) }
+
+        val edges = listOf(
+            Edge(vertices[0], vertices[1], 1),
+            Edge(vertices[0], vertices[2], 2),
+            Edge(vertices[0], vertices[3], 3),
+            Edge(vertices[1], vertices[2], 1),
+            Edge(vertices[1], vertices[3], 1),
+            Edge(vertices[2], vertices[3], 1)
+        )
+
+        testAllPaths(edges, vertices[0], vertices[3], listOf(
+            ExpectedPath(2, 2),
+            ExpectedPath(3, 1),
+            ExpectedPath(3, 2),
+            ExpectedPath(3, 3)))
+    }
+
+
+    data class ExpectedPath(val cost: Int, val edges: Int)
+
+    private fun testAllPaths(
+        edges: List<Edge>,
+        start: Vertex,
+        finish: Vertex,
+        expectedPaths: List<ExpectedPath>
+    ) {
+        val result = allPaths(edges, start, finish)
+
+        result.forEach() {
+            println("Cost ${it.cost}")
+            println("Edges ${it.history.map { edge -> edge.destination }}")
+        }
+
+        val actualPaths = result.map { it -> ExpectedPath(it.cost, it.history.size) }
+
+        assertThat(actualPaths, `is`(expectedPaths))
+    }
+
+    private fun testShortestPath(
+        edges: List<Edge>,
+        start: Vertex,
+        finish: Vertex,
+        expectedCost: Int,
+        expectedEdges: Int
+    ) {
+        val result = shortestPath(edges, start, finish)
+        println("Cost ${result.cost}")
+        println("Edges ${result.history.map { it -> it.destination }}")
+        assertThat(result.cost, equalTo(expectedCost))
+        assertThat(result.history.size, equalTo(expectedEdges))
+    }
+
+
+    fun shortestPath(edges: List<Edge>, start: Vertex, finish: Vertex) : GraphSearch.Path<Vertex> {
+
+        val duplexEdges = edges.plus(edges.map { Edge(it.v2, it.v1, it.cost) })
+
+        fun generateActions(vertex: Vertex) = duplexEdges.filter { it.v1 == vertex }
+            .map { GraphSearch.Edge(it.cost, it.v2) }
+
+        return GraphSearch.shortestPath(start, { v -> v == finish }, { v -> generateActions(v) })
+    }
+
+    fun allPaths(edges: List<Edge>, start: Vertex, finish: Vertex) : List<GraphSearch.Path<Vertex>> {
+
+        val duplexEdges = edges.plus(edges.map { Edge(it.v2, it.v1, it.cost) })
+
+        fun generateActions(vertex: Vertex) = duplexEdges.filter { it.v1 == vertex }
+            .map { GraphSearch.Edge(it.cost, it.v2) }
+
+        return GraphSearch.allPaths(start, { v -> v == finish }, { v -> generateActions(v) })
+    }
+
+}
